3a955a6b26082e50cbc13a9789aa93e9ccfcbc5b
Macchiatobin <chanbin0502@naver.com>
2023-05-23 12:11:02 +0800

    Manager类里调用Database类的接口

--- src/main/java/cn/edu/thssdb/schema/Manager.java
+++ src/main/java/cn/edu/thssdb/schema/Manager.java
@@ -15,12 +15,35 @@ public class Manager {
     // TODO
   }
 
-  private void createDatabaseIfNotExists() {
-    // TODO
+  private void createDatabaseIfNotExists(String databaseName) {
+    /* macchiato code start */
+    if (databases.get(databaseName) != null) // exists already
+    {
+      System.out.println("Database exists already!");
+      return;
+      //TODO: 修改报错形式以及返回状态
+    }
+
+    Database newDB = new Database(databaseName);
+    databases.put(databaseName, newDB);
+    /* macchiato code end */
+
+    // TODO: 应该返回成功状态什么的...
   }
 
-  private void deleteDatabase() {
-    // TODO
+  private void deleteDatabase(String databaseName) {
+    /* macchiato code start */
+    if (databases.get(databaseName) == null)
+    {
+      System.out.println("Database not existing");
+      return;
+      //TODO: 修改报错形式以及返回状态
+    }
+
+    databases.remove(databaseName);
+    /* macchiato code end */
+
+    //TODO
   }
 
   public void switchDatabase() {
