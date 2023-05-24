4f615cbb258a0272f3269899e1206e46e42a7f9b
AmyXin <amyxx2001@163.com>
2023-05-24 21:10:27 +0800

    remove extra files

--- exception/ValueFormatException.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.thssdb.exception;
-
-public class ValueFormatException extends RuntimeException {
-	
-	@Override
-	public String getMessage() {
-			return "Exception: Value format mismatched!";
-		}
-}
