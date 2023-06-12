68a2d3897c45b703d5be256e7d375766846beca8
rayheng030 <rokmeng@gmail.com>
2023-06-12 20:21:28 +0800

    transaction changed

--- src/main/java/cn/edu/thssdb/plan/impl/BeginTransactionPlan.java
+++ src/main/java/cn/edu/thssdb/plan/impl/BeginTransactionPlan.java
@@ -2,9 +2,6 @@ package cn.edu.thssdb.plan.impl;
 
 import cn.edu.thssdb.plan.LogicalPlan;
 import cn.edu.thssdb.rpc.thrift.ExecuteStatementResp;
-import cn.edu.thssdb.schema.Database;
-import cn.edu.thssdb.schema.Manager;
-import cn.edu.thssdb.transaction.MainTransaction;
 
 public class BeginTransactionPlan extends LogicalPlan {
 
@@ -20,10 +17,6 @@ public class BeginTransactionPlan extends LogicalPlan {
   @Override
   public ExecuteStatementResp execute_plan() {
     // TODO
-    Manager manager = Manager.getInstance();
-    Database database = manager.getCurDB();
-    String databaseName = database.getdatabaseName();
-    MainTransaction mainTransaction = new MainTransaction(databaseName);
     return null;
   }
 }
